3e39966eda9138f4d266f795c0839a638d9d02e6
CookieWookiee <31262615+CookieWookiee@users.noreply.github.com>
2017-10-17 01:51:10 -0400

    0.1.1.3251
    Adds a check for whether a trader is Orbital/Base or Visitor/Caravan and adds a random offset to the scalar.

--- SupplyandDemand.cs
+++ SupplyandDemand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Collections.Generic;
 using System.Diagnostics;
 using RimWorld;
@@ -7,7 +8,7 @@ using HugsLib;
 using Harmony;
 using Verse;
 using UnityEngine;
-
+[assembly:AssemblyVersion("0.1.1.*")] //Syntax: [Game Major] [Mod Major] [Game Minor] [Mod Minor]
 
 namespace TradeScalar
 {
@@ -20,7 +21,12 @@ namespace TradeScalar
                 return "Supply_And_Demand";
             }
         }
-        [HarmonyPatch(typeof(StockGenerator), "RandomCountOf", null)]
+        private TradeScalar()
+        {
+            
+        }
+        [HarmonyPatch(typeof(StockGenerator))]
+        [HarmonyPatch("RandomCountOf")]
         ///<summary>
         ///Defines the method for scaling the result of RandomCountOf by colony wealth and difficulty level.
         ///</summary>
@@ -30,14 +36,21 @@ namespace TradeScalar
             //public static void Conditioner()
             //{
 
+
             //}
             [HarmonyPostfix]
-            public static void Scaled(ref int __result)
+            public static void Scaled(ref int __result, StockGenerator __instance)
             {
-                int wealthTotal = Mathf.RoundToInt(Find.VisibleMap.wealthWatcher.WealthTotal); //Uses Verse.Find to get colony wealth, rounded to an integer.
-                float tradeLoss = (1f - Find.Storyteller.difficulty.tradePriceFactorLoss); //Finds the tradePriceFactorLoss. Value is 1, 0.95, 0.9, 0.85, or 0.8
-                __result = Mathf.RoundToInt((float)Math.Log(Math.E + (wealthTotal * tradeLoss)) * __result); //Minimum value of scalar is 1 when wealthTotal = 0
+                //TradeScalar tradeScalar = new TradeScalar();
+                //var placeholder = __instance.trader.defName;
+                if (__instance.trader.orbital || __instance.trader.defName.Contains("Base_"))
+                {
+                    int random = UnityEngine.Random.Range(-3, 3);
+                    int wealthTotal = Mathf.RoundToInt(Find.VisibleMap.wealthWatcher.WealthTotal); //Uses Verse.Find to get colony wealth, rounded to an integer.
+                    float tradeLoss = (1f - Find.Storyteller.difficulty.tradePriceFactorLoss); //Finds the tradePriceFactorLoss. Value is 1, 0.95, 0.9, 0.85, or 0.8
+                    __result = random + Mathf.RoundToInt((float)Math.Log(Math.E + (wealthTotal * tradeLoss)) * __result); //Minimum value of scalar is 1 when wealthTotal = 0
+                }              
             }
-        }
+        }        
     }
 }
